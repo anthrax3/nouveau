740b9f0af15df6b99d5244514c8251d4348cce3d
Karol Herbst <karolherbst@gmail.com>
2016-07-12 21:36:09 +0200

    clk: We should pass the pstate id around not the index in the list
    This makes the code easier, because we can compare the id with
    pstate->pstate and saves us from the trouble of iterating over the pstates
    to match the index.
    
    v2: reword commit message
    
    Signed-off-by: Karol Herbst <karolherbst@gmail.com>
    Reviewed-by: Martin Peres <martin.peres@free.fr>

--- drm/nouveau/nvkm/subdev/clk/base.c
+++ drm/nouveau/nvkm/subdev/clk/base.c
@@ -273,23 +273,26 @@ nvkm_cstate_new(struct nvkm_clk *clk, int idx, struct nvkm_pstate *pstate)
  * P-States
  *****************************************************************************/
 static int
-nvkm_pstate_prog(struct nvkm_clk *clk, int pstate_idx)
+nvkm_pstate_prog(struct nvkm_clk *clk, int pstate_id)
 {
 	struct nvkm_subdev *subdev = &clk->subdev;
 	struct nvkm_fb *fb = subdev->device->fb;
 	struct nvkm_pci *pci = subdev->device->pci;
 	struct nvkm_pstate *pstate;
-	int ret, idx = 0;
+	int ret;
 
-	if (pstate_idx == NVKM_CLK_PSTATE_DEFAULT)
+	if (pstate_id == NVKM_CLK_PSTATE_DEFAULT)
 		return 0;
 
 	list_for_each_entry(pstate, &clk->states, head) {
-		if (idx++ == pstate_idx)
+		if (pstate->id == pstate_id)
 			break;
 	}
 
-	nvkm_debug(subdev, "setting performance state %d\n", pstate_idx);
+	if (!pstate)
+		return -EINVAL;
+
+	nvkm_debug(subdev, "setting performance state %x\n", pstate_id);
 	clk->pstate = pstate;
 
 	nvkm_pcie_set_link(pci, pstate->pcie_speed, pstate->pcie_width);
@@ -313,34 +316,33 @@ nvkm_clk_update_work(struct work_struct *work)
 {
 	struct nvkm_clk *clk = container_of(work, typeof(*clk), work);
 	struct nvkm_subdev *subdev = &clk->subdev;
-	int pstate_idx;
+	int pstate_id;
 
 	if (!atomic_xchg(&clk->waiting, 0))
 		return;
 	clk->pwrsrc = power_supply_is_system_supplied();
 
 	if (clk->pstate)
-		pstate_idx = clk->pstate->id;
+		pstate_id = clk->pstate->id;
 	else
-		pstate_idx = NVKM_CLK_PSTATE_DEFAULT;
+		pstate_id = NVKM_CLK_PSTATE_DEFAULT;
 	nvkm_trace(subdev, "P %d PWR %d U(AC) %d U(DC) %d A %d C %d T %d°C\n",
-		   pstate_idx, clk->pwrsrc, clk->ustate_ac, clk->ustate_dc,
+		   pstate_id, clk->pwrsrc, clk->ustate_ac, clk->ustate_dc,
 		   clk->astate, clk->exp_cstateid, clk->temp);
 
-	pstate_idx = clk->pwrsrc ? clk->ustate_ac : clk->ustate_dc;
-	if (clk->state_nr && pstate_idx != NVKM_CLK_PSTATE_DEFAULT) {
-		pstate_idx = (pstate_idx < 0) ? clk->astate : pstate_idx;
-		pstate_idx = min(pstate_idx, clk->state_nr - 1);
+	pstate_id = clk->pwrsrc ? clk->ustate_ac : clk->ustate_dc;
+	if (clk->state_nr && pstate_id != NVKM_CLK_PSTATE_DEFAULT) {
+		pstate_id = (pstate_id < 0) ? clk->astate : pstate_id;
 	} else {
-		pstate_idx = NVKM_CLK_PSTATE_DEFAULT;
+		pstate_id = NVKM_CLK_PSTATE_DEFAULT;
 	}
 
-	nvkm_trace(subdev, "-> %d\n", pstate_idx);
-	if (!clk->pstate || pstate_idx != clk->pstate->id) {
-		int ret = nvkm_pstate_prog(clk, pstate_idx);
+	nvkm_trace(subdev, "-> %d\n", pstate_id);
+	if (!clk->pstate || pstate_id != clk->pstate->id) {
+		int ret = nvkm_pstate_prog(clk, pstate_id);
 		if (ret) {
 			nvkm_error(subdev, "error setting pstate %d: %d\n",
-				   pstate_idx, ret);
+				   pstate_id, ret);
 		}
 	}
 
@@ -492,32 +494,9 @@ nvkm_pstate_new(struct nvkm_clk *clk, int idx)
  * Adjustment triggers
  *****************************************************************************/
 static int
-nvkm_clk_ustate_update(struct nvkm_clk *clk, int req)
-{
-	struct nvkm_pstate *pstate;
-	int i = 0;
-
-	if (!clk->allow_reclock)
-		return -ENOSYS;
-
-	if (req != -1 && req != -2) {
-		list_for_each_entry(pstate, &clk->states, head) {
-			if (pstate->id == req)
-				break;
-			i++;
-		}
-
-		if (pstate->id != req)
-			return -EINVAL;
-		req = i;
-	}
-
-	return req + 2;
-}
-
-static int
 nvkm_clk_nstate(struct nvkm_clk *clk, const char *mode, int arglen)
 {
+	struct nvkm_pstate *pstate;
 	int ret = 1;
 
 	if (clk->allow_reclock && !strncasecmpz(mode, "auto", arglen))
@@ -529,27 +508,46 @@ nvkm_clk_nstate(struct nvkm_clk *clk, const char *mode, int arglen)
 
 		((char *)mode)[arglen] = '\0';
 		if (!kstrtol(mode, 0, &v)) {
-			ret = nvkm_clk_ustate_update(clk, v);
+			ret = v;
 			if (ret < 0)
 				ret = 1;
 		}
 		((char *)mode)[arglen] = save;
 	}
 
-	return ret - 2;
+	if (ret < 0)
+		return ret;
+
+	list_for_each_entry(pstate, &clk->states, head) {
+		if (pstate->id == ret)
+			return ret;
+	}
+	return -EINVAL;
 }
 
 int
 nvkm_clk_ustate(struct nvkm_clk *clk, int req, int pwr)
 {
-	int ret = nvkm_clk_ustate_update(clk, req);
-	if (ret >= 0) {
-		if (ret -= 2, pwr) clk->ustate_ac = ret;
-		else		   clk->ustate_dc = ret;
-		clk->exp_cstateid = NVKM_CLK_CSTATE_HIGHEST;
-		return nvkm_clk_update(clk, true);
+	struct nvkm_pstate *pstate;
+	bool valid = false;
+
+	list_for_each_entry(pstate, &clk->states, head) {
+		if (pstate->id == req) {
+			valid = true;
+			break;
+		}
 	}
-	return ret;
+
+	if (!valid)
+		return -EINVAL;
+
+	if (pwr)
+		clk->ustate_ac = req;
+	else
+		clk->ustate_dc = req;
+
+	clk->exp_cstateid = NVKM_CLK_CSTATE_HIGHEST;
+	return nvkm_clk_update(clk, true);
 }
 
 int
@@ -628,7 +626,7 @@ nvkm_clk_init(struct nvkm_subdev *subdev)
 	if (clk->func->init)
 		return clk->func->init(clk);
 
-	clk->astate = clk->state_nr - 1;
+	clk->astate = NVKM_CLK_PSTATE_DEFAULT;
 	clk->pstate = NULL;
 	clk->exp_cstateid = NVKM_CLK_CSTATE_DEFAULT;
 	clk->cstate = NULL;
